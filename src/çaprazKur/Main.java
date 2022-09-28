7e040297f50ff6114e0ec72ca79e43b567867e9b
emirhan1414 <113902435+emirhan1414@users.noreply.github.com>
2022-09-28 12:04:14 +0300

    Initial commit

---
+++ src/çaprazKur/Main.java
@@ -0,0 +1,16 @@
+package çaprazKur;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double dolar=18.45
+		double euro=17.89
+		
+		double dovizMiktarı=10
+		
+		double tlMiktar=dolar * dovizMiktar;
+		System.out.println("EURO MİKTAR"); 
+
+	}
+
+}
